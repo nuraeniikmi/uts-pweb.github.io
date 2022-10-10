391b5bde3036ef83a47e06c7895c6629ee428cc8
nuraeni <97010650+nuraeniikmi@users.noreply.github.com>
2022-10-10 22:18:06 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const txtElement = ['Saya Mahasiswa STMIK IKMI', 'NIM Saya 41215739', 'Saya Kelas TI KIP P4'];
+let count = 0;
+let txtIndex = 0;
+let currentTxt = '';
+let words = '';
+
+(function ngetik(){
+
+	if(count == txtElement.length){
+		count = 0;
+	}
+
+	currentTxt = txtElement[count];
+
+	words = currentTxt.slice(0, ++txtIndex);
+	document.querySelector('.efek-ngetik').textContent = words;
+
+	if(words.length == currentTxt.length){
+		count++;
+		txtIndex = 0;
+	}
+
+	setTimeout(ngetik, 500);
+
+})();
